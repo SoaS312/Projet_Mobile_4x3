8a78f3bd0d6b8cbf4fa032f11877d870b3ae0291
Joris <j.fabre@rubika-edu.com>
2019-01-31 16:41:58 +0100

    SelectionLevelProposition

---
+++ Assets/ClickingArea.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickingArea : MonoBehaviour
+
+{
+    public bool zoomIn = false;
+    public int ZoomFOV;
+    public int speed;
+    public Camera cam;
+
+    private void Awake()
+    {
+        cam = this.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        ZoomToRegion();
+
+        if (zoomIn)
+        {
+            if (cam.fieldOfView > ZoomFOV)
+            {
+               cam.fieldOfView -= speed * Time.deltaTime;
+            }
+
+            if(cam.fieldOfView <= ZoomFOV)
+            {
+                GetComponentInChildren<ParticleSystem>().Stop();
+            }
+        }
+    }
+
+    private void ZoomToRegion()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "America")
+                {
+                    zoomIn = true;
+                    GetComponentInChildren<ParticleSystem>().Play();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/DragScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+
+    public float rotSpeed = 20;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
+
+        //transform.RotateAround(Vector3.up, -rotX);
+        //transform.RotateAround(Vector3.right, rotY);
+
+        transform.Rotate(Vector3.up, -rotX, Space.World);
+        transform.Rotate(Vector3.right,rotY, Space.World);
+    }
+}
