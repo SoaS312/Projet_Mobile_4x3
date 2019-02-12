c06e7d70f623b2af0596682ce8e707c33ee305ab
Joris <j.fabre@rubika-edu.com>
2019-02-12 14:31:14 +0100

    Test Thomas Desaunay

---
+++ Assets/OnTouchChangeRole.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTouchChangeRole : MonoBehaviour
+{
+    public GameObject hittedObject;
+    public  UnityEvent BoolEvent;
+
+    private void Awake()
+    {
+        hittedObject = this.gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        { 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.gameObject == hittedObject)
+                {
+                    BoolEvent.Invoke();
+                }
+            }
+        }
+    }
+}
