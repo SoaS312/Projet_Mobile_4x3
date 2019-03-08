73aebb08d19b78fad4394aabafc97a642796c1df
Joris <j.fabre@rubika-edu.com>
2019-03-08 14:29:56 +0100

    Test build mobile

---
+++ Assets/loadScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+    public Object SceneToLoad;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > (0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == gameObject.transform.name)
+                {
+                    SceneManager.LoadScene(SceneToLoad.name);
+                }
+            }
+        }
+    }
+}
