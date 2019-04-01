c2f7e60ce26472cbb983c88c09bfd0d014fb6d41
Jules Klein <jules.klein159@gmail.com>
2019-04-01 16:46:18 +0200

    Jules_UI
    Menu Pause_UI

--- Assets/Scripts/Manager/PauseMenu.cs
+++ Assets/Scripts/Manager/PauseMenu.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-
+    public GameObject PausedMenu;
     public bool isPaused;
+
+    private Scene scene;
 
     void Start()
     {
         isPaused = false;
         Time.timeScale = 1f;
+        PausedMenu.SetActive(false);
+        scene = SceneManager.GetActiveScene();
     }
 
 
@@ -22,10 +26,23 @@ public class PauseMenu : MonoBehaviour
         if (isPaused)
         {
             Time.timeScale = 0f;
+            PausedMenu.SetActive(true);
         }
         if (!isPaused)
         {
             Time.timeScale = 1f;
+            PausedMenu.SetActive(false);
         }
     }
+
+    public void ResetLevel()
+    {
+        Application.LoadLevel(scene.name);
+    }
+    
+    public void QuitLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
