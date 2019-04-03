7dc66d9550c9538bf96ca5da2259b24e83f52bf3
Joris <j.fabre@rubika-edu.com>
2019-04-03 11:09:41 +0200

    Push Scene

--- Assets/Scripts/Task/Conducteur.cs
+++ Assets/Scripts/Task/Conducteur.cs
@@ -23,7 +23,7 @@ public class Conducteur : MonoBehaviour
     {
         if (FoodTruckState.staticFoodTruckState.isDriverActive)
         {
-            if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+            /*if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
@@ -39,19 +39,19 @@ public class Conducteur : MonoBehaviour
                     }
 
                 }
-            }
-           /* if (SwipeLogger.staticSwipeLogger.stockedDirection == "Up" && RouteManager.staticRouteManager.Index < RouteManager.staticRouteManager.VoiesRoutes.Count && actualTimer <= 0)
+            }*/
+           if (SwipeLogger.staticSwipeLogger.stockedDirection == "Left" && RouteManager.staticRouteManager.Index < RouteManager.staticRouteManager.VoiesRoutes.Count && actualTimer <= 0)
             {
                 RouteManager.staticRouteManager.Index += 1;
                 actualTimer = MaxTimer;
                 SwipeLogger.staticSwipeLogger.stockedDirection = "clean";
             }
-            if (SwipeLogger.staticSwipeLogger.stockedDirection == "Down" && RouteManager.staticRouteManager.Index > 0 && actualTimer <= 0)
+            if (SwipeLogger.staticSwipeLogger.stockedDirection == "Right" && RouteManager.staticRouteManager.Index > 0 && actualTimer <= 0)
             {
                 RouteManager.staticRouteManager.Index -= 1;
                 actualTimer = MaxTimer;
                 SwipeLogger.staticSwipeLogger.stockedDirection = "clean";
-            } */
+            }
         }
     }
 }
