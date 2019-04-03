59d6652601781628dee9c52250d2f3f13b8e3928
SoaS312 <j.fabre@rubika-edu.com>
2019-04-03 23:21:58 +0200

    lol la fatigue

---
+++ Assets/Scripts/FPSDisplayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSDisplayer : MonoBehaviour
+/*{
+
+    private int FramesPerSec;
+    private float frequency = 1.0f;
+    private string fps;
+
+
+
+    void Start()
+    {
+        StartCoroutine(FPS());
+    }
+
+    private IEnumerator FPS()
+    {
+        for (; ; )
+        {
+            // Capture frame-per-second
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            fps = string.Format("FPS: {0}", Mathf.RoundToInt(frameCount / timeSpan));
+            Debug.Log(fps);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 100, 10, 150, 20), fps);
+    }
+}*/
+{
+    float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        GUI.Label(rect, text, style);
+    }
+}
