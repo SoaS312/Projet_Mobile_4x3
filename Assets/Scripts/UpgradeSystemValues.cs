c97806e388d5cd4c1dd62eb3d432a12c0e675f4c
Joris <j.fabre@rubika-edu.com>
2019-04-30 20:47:58 +0200

    Up

--- Assets/Scripts/UpgradeSystemValues.cs
+++ Assets/Scripts/UpgradeSystemValues.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class UpgradeSystemValues : MonoBehaviour
 {
     public static GameObject UpgradeSystemValuesHolder;
 
+    // Dans le code du controlleur correspondant =>   valeur == upgradesystemvalues.jobUpgradeList[index];
+
+    [Header("Driver")]
+    public static int DriverLevelIndex;
+    public int DriverLevelIndexLOOK; // A dégager quand l'upgrade fonctionnera bien
+    public List<int> DriverUpgradeList; // ça change
+
     [Header("Cook")]
-    public List<int> MaxCookingProgressList;
-    public List<int> UsedFoodByClickList;
+    public static int CookLevelIndex;
+    public int CookLevelIndexLOOK;// A dégager quand l'upgrade fonctionnera bien
+    public List<int> CookUpgradeList; //
+
+    [Header("Seller")]
+    public static int SellerLevelIndex;
+    public int SellerLevelIndexLOOK;// A dégager quand l'upgrade fonctionnera bien
+    public List<int> SellerUpgradeList; //
+
+    [Header("Vigile")]
+    public static int VigileLevelIndex;
+    public int VigileLevelIndexLOOK;// A dégager quand l'upgrade fonctionnera bien
+    public List<int> VigileUpgradeList; //
+
+    [Header("Helico")]
+    public static int HelicoLevelIndex;
+    public int HelicoLevelIndexLOOK;// A dégager quand l'upgrade fonctionnera bien
+    public List<int> HelicoUpgradeList; //
+
+    [Header("Scooter")]
+    public static int ScooterLevelIndex;
+    public int ScooterLevelIndexLOOK;// A dégager quand l'upgrade fonctionnera bien
+    public List<int> ScooterUpgradeList; //
 
     public void Awake()
     {
         DontDelete();
+    }
+
+    public void Update()
+    {
+        DriverLevelIndexLOOK = DriverLevelIndex;
     }
 
     private void DontDelete()
@@ -27,4 +61,24 @@ public class UpgradeSystemValues : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+[Button]
+private void UpgradeDriver()
+    {
+        if (DriverLevelIndex < 100)
+        DriverLevelIndex += 1;
+    }
+
+    [Button]
+    private void ResetDriver()
+    {
+        DriverLevelIndex = 0;
+    }
+
+
+
+
+
+
+
 }
