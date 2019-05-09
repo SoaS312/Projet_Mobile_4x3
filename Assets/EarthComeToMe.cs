7d43c71997aa26daca0751e70ebd2e2782aa417e
Joris <j.fabre@rubika-edu.com>
2019-05-09 12:15:06 +0200

    EarthMan

---
+++ Assets/EarthComeToMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthComeToMe : MonoBehaviour
+{
+    public bool Coming = false;
+    public Vector3 DesiredPos;
+    public float speed;
+
+    public void Come()
+    {
+        Coming = true;
+    }
+
+    public void Update()
+    {
+        if (Coming && transform.position.z > DesiredPos.z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z - speed));
+        }
+
+        if (transform.position.z < 40)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 40);
+        }
+    }
+}
