2a0f9de06ad57748939fb2f89c8c3f0e20c4d807
Joris <j.fabre@rubika-edu.com>
2019-05-14 21:32:01 +0200

    ....

---
+++ Assets/MainMenuValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuValue : MonoBehaviour
+{
+
+    public MainTitleCheck MainTitleManager;
+
+    public void Awake()
+    {
+        MainTitleManager.MainMenuSkipped = false;
+    }
+}
