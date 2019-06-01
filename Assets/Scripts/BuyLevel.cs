d19a89c9f13f3f4b17c19056a38d4ebf8471fd6f
Joris <j.fabre@rubika-edu.com>
2019-06-01 11:58:42 +0200

    UI

--- Assets/Scripts/BuyLevel.cs
+++ Assets/Scripts/BuyLevel.cs
@@ -10,6 +10,12 @@ public class BuyLevel : MonoBehaviour
     public CrewUpgradeScriptable DriverCrew;
     public List<int> DriverCost;
     public TMP_Text DriverCostText;
+
+    [Header("Driver Prenium")]
+    public List<int> DriverCostPrenium;
+    public TMP_Text DriverCostPreniumText;
+
+    [Header("Driver Level")]
     public TMP_Text DriverLevel;
 
     [Header("Cook")]
@@ -17,25 +23,60 @@ public class BuyLevel : MonoBehaviour
     public List<int> CookCost;
     public TMP_Text CookCostText;
 
+    [Header("Cook Prenium")]
+    public List<int> CookCostPrenium;
+    public TMP_Text CookCostPreniumText;
+
+    [Header("Cook Level")]
+    public TMP_Text CookLevel;
+
     [Header("Vigile")]
     public CrewUpgradeScriptable VigileCrew;
     public List<int> VigileCost;
     public TMP_Text VigileCostText;
+
+    [Header("Vigile Prenium")]
+    public List<int> VigileCostPrenium;
+    public TMP_Text VigileCostPreniumText;
+
+    [Header("Vigile Level")]
+    public TMP_Text VigileLevel;
 
     [Header("Seller")]
     public CrewUpgradeScriptable SellerCrew;
     public List<int> SellerCost;
     public TMP_Text SellerCostText;
 
+    [Header("Seller Prenium")]
+    public List<int> SellerCostPrenium;
+    public TMP_Text SellerCostPreniumText;
+
+    [Header("Seller Level")]
+    public TMP_Text SellerLevel;
+
     [Header("Helico")]
     public CrewUpgradeScriptable HelicoCrew;
     public List<int> HelicoCost;
     public TMP_Text HelicoCostText;
 
+    [Header("Helico Prenium")]
+    public List<int> HelicoCostPrenium;
+    public TMP_Text HelicoCostPreniumText;
+
+    [Header("Helico Level")]
+    public TMP_Text HelicoLevel;
+
     [Header("Scooter")]
     public CrewUpgradeScriptable ScooterCrew;
     public List<int> ScooterCost;
     public TMP_Text ScooterCostText;
+
+    [Header("Scooter Prenium")]
+    public List<int> ScooterCostPrenium;
+    public TMP_Text ScooterCostPreniumText;
+
+    [Header("Scooter Level")]
+    public TMP_Text ScooterLevel;
 
     public void OnEnable()
     {
@@ -46,18 +87,32 @@ public class BuyLevel : MonoBehaviour
         HelicoCrew.CrewLevelIndex = PlayerPrefs.GetInt("HelicoLevel");
         ScooterCrew.CrewLevelIndex = PlayerPrefs.GetInt("ScooterLevel");
         DriverLevel.text = "Level " + DriverCrew.CrewLevelIndex +":";
+        CookLevel.text = "Level " + CookCrew.CrewLevelIndex + ":";
+        SellerLevel.text = "Level " + SellerCrew.CrewLevelIndex + ":";
+        VigileLevel.text = "Level " + VigileCrew.CrewLevelIndex + ":";
+        HelicoLevel.text = "Level " + HelicoCrew.CrewLevelIndex + ":";
+        ScooterLevel.text = "Level " + ScooterCrew.CrewLevelIndex + ":";
     }
 
     public void Update()
     {
         DriverCostText.text = "" + DriverCost[DriverCrew.CrewLevelIndex];
+        DriverCostPreniumText.text = "" + DriverCostPrenium[DriverCrew.CrewLevelIndex];
+
         CookCostText.text = "" + CookCost[CookCrew.CrewLevelIndex];
+        CookCostPreniumText.text = "" + CookCostPrenium[CookCrew.CrewLevelIndex];
 
         VigileCostText.text = "" + VigileCost[VigileCrew.CrewLevelIndex];
+        VigileCostPreniumText.text = "" + VigileCostPrenium[VigileCrew.CrewLevelIndex];
+
         SellerCostText.text = "" + SellerCost[SellerCrew.CrewLevelIndex];
+        SellerCostPreniumText.text = "" + SellerCostPrenium[SellerCrew.CrewLevelIndex];
 
         HelicoCostText.text = "" + HelicoCost[HelicoCrew.CrewLevelIndex];
+        HelicoCostPreniumText.text = "" + HelicoCostPrenium[HelicoCrew.CrewLevelIndex];
+
         ScooterCostText.text = "" + ScooterCost[ScooterCrew.CrewLevelIndex];
+        ScooterCostPreniumText.text = "" + ScooterCostPrenium[ScooterCrew.CrewLevelIndex];
     }
 
     public void BuyStandardDriver()
@@ -68,6 +123,23 @@ public class BuyLevel : MonoBehaviour
             DriverCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("DriverLevel", DriverCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            DriverLevel.text = "Level " + DriverCrew.CrewLevelIndex + ":";
+        }
+        else
+        {
+            Debug.Log("Vibrate"); // petit effet erreur/pas possible
+        }
+    }
+
+    public void BuyPreniumDriver()
+    {
+        DriverCrew.CrewLevelIndex = PlayerPrefs.GetInt("DriverLevel");
+        if (MoneyManager.PreniumMoney >= DriverCostPrenium[DriverCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= DriverCostPrenium[DriverCrew.CrewLevelIndex];
+            DriverCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("DriverLevel", DriverCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
             DriverLevel.text = "Level " + DriverCrew.CrewLevelIndex + ":";
         }
     }
@@ -81,6 +153,20 @@ public class BuyLevel : MonoBehaviour
             CookCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("CookLevel", CookCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            CookLevel.text = "Level " + CookCrew.CrewLevelIndex + ":";
+        }
+    }
+
+    public void BuyPreniumCook()
+    {
+        CookCrew.CrewLevelIndex = PlayerPrefs.GetInt("CookLevel");
+        if (MoneyManager.PreniumMoney >= CookCostPrenium[CookCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= CookCostPrenium[CookCrew.CrewLevelIndex];
+            CookCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("CookLevel", CookCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
+            CookLevel.text = "Level " + CookCrew.CrewLevelIndex + ":";
         }
     }
 
@@ -93,6 +179,20 @@ public class BuyLevel : MonoBehaviour
             VigileCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("VigileLevel", VigileCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            VigileLevel.text = "Level " + VigileCrew.CrewLevelIndex + ":";
+        }
+    }
+
+    public void BuyPreniumVigile()
+    {
+        VigileCrew.CrewLevelIndex = PlayerPrefs.GetInt("VigileLevel");
+        if (MoneyManager.PreniumMoney >= VigileCostPrenium[VigileCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= VigileCostPrenium[VigileCrew.CrewLevelIndex];
+            VigileCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("VigileLevel", CookCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
+            VigileLevel.text = "Level " + VigileCrew.CrewLevelIndex + ":";
         }
     }
 
@@ -105,6 +205,20 @@ public class BuyLevel : MonoBehaviour
             SellerCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("SellerLevel", SellerCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            SellerLevel.text = "Level " + SellerCrew.CrewLevelIndex + ":";
+        }
+    }
+
+    public void BuyPreniumSeller()
+    {
+       SellerCrew.CrewLevelIndex = PlayerPrefs.GetInt("SellerLevel");
+        if (MoneyManager.PreniumMoney >= SellerCostPrenium[SellerCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= SellerCostPrenium[SellerCrew.CrewLevelIndex];
+            SellerCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("SellerLevel", SellerCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
+            SellerLevel.text = "Level " + SellerCrew.CrewLevelIndex + ":";
         }
     }
 
@@ -117,6 +231,20 @@ public class BuyLevel : MonoBehaviour
             HelicoCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("HelicoLevel", HelicoCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            HelicoLevel.text = "Level " + HelicoCrew.CrewLevelIndex + ":";
+        }
+    }
+
+    public void BuyPreniumHelico()
+    {
+        HelicoCrew.CrewLevelIndex = PlayerPrefs.GetInt("HelicoLevel");
+        if (MoneyManager.PreniumMoney >= HelicoCostPrenium[HelicoCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= HelicoCostPrenium[HelicoCrew.CrewLevelIndex];
+            HelicoCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("HelicoLevel", HelicoCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
+            HelicoLevel.text = "Level " + HelicoCrew.CrewLevelIndex + ":";
         }
     }
 
@@ -130,6 +258,20 @@ public class BuyLevel : MonoBehaviour
             ScooterCrew.CrewLevelIndex += 1;
             PlayerPrefs.SetInt("ScooterLevel", ScooterCrew.CrewLevelIndex);
             PlayerPrefs.SetInt("StandardMoney", MoneyManager.StandardMoney);
+            ScooterLevel.text = "Level " + ScooterCrew.CrewLevelIndex + ":";
+        }
+    }
+
+    public void BuyPreniumScooter()
+    {
+        ScooterCrew.CrewLevelIndex = PlayerPrefs.GetInt("ScooterLevel");
+        if (MoneyManager.PreniumMoney >= ScooterCostPrenium[ScooterCrew.CrewLevelIndex])
+        {
+            MoneyManager.PreniumMoney -= ScooterCostPrenium[ScooterCrew.CrewLevelIndex];
+            ScooterCrew.CrewLevelIndex += 1;
+            PlayerPrefs.SetInt("ScooterLevel", ScooterCrew.CrewLevelIndex);
+            PlayerPrefs.SetInt("PreniumMoney", MoneyManager.PreniumMoney);
+            ScooterLevel.text = "Level " + ScooterCrew.CrewLevelIndex + ":";
         }
     }
 }
--- Assets/Scripts/ShowMoney.cs
+++ Assets/Scripts/ShowMoney.cs
@@ -7,15 +7,18 @@ public class ShowMoney : MonoBehaviour
 {
 
     public TMP_Text StandardMoneyText;
+    public TMP_Text PreniumMoneyText;
 
     private void Start()
     {
         MoneyManager.StandardMoney = PlayerPrefs.GetInt("StandardMoney");
+        MoneyManager.PreniumMoney = PlayerPrefs.GetInt("PreniumMoney");
     }
 
 
     void Update()
     {
         StandardMoneyText.text = "" + MoneyManager.StandardMoney;
+        PreniumMoneyText.text = "" + MoneyManager.PreniumMoney;
     }
 }
