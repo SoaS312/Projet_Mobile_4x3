acf041f3c779116c93b5bbaa0985a5237fb4d1fb
Joris <j.fabre@rubika-edu.com>
2019-06-10 17:47:29 +0200

    ...___...

---
+++ Assets/OpenRealMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenRealMap : MonoBehaviour
+{
+    public GameObject Map;
+
+    public void Openmap()
+    {
+        Map.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
