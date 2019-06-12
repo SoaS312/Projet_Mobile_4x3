f6d4ffecc616f908fa536c58bd0f981b62e4e11f
Joris <j.fabre@rubika-edu.com>
2019-06-12 19:33:20 +0200

    ...

---
+++ Assets/ChangeSkin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkin : MonoBehaviour
+{
+    public List<MeshRenderer> TruckMaterials;
+    public Material StartMaterial;
+
+    void Start()
+    {
+        SkinHolder.TruckMaterial = StartMaterial;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < TruckMaterials.Count; i++)
+        {
+            TruckMaterials[i].material = SkinHolder.TruckMaterial;
+        }
+    }
+}
---
+++ Assets/SkinSending.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSending : MonoBehaviour
+{
+    public Material Skin;
+
+    public void SendSkin()
+    {
+        SkinHolder.TruckMaterial = Skin;
+    }
+}
