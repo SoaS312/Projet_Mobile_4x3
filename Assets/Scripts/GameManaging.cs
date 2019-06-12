36d322a0a47937bece56bc01adbb79fe1f356846
Joris <j.fabre@rubika-edu.com>
2019-06-12 11:47:14 +0200

    /!\

--- Assets/Scripts/GameManaging.cs
+++ Assets/Scripts/GameManaging.cs
@@ -5,6 +5,77 @@ using UnityEngine;
 public class GameManaging : MonoBehaviour
 {
     public static int NeverSleep;
+    public GameObject WarningFuelGauche, WarningFuelDroite, WarningFoodGauche, WarningFoodDroite, WarningBothGauche, WarningBothDroite;
+
+    public void Update()
+    {
+
+        if (FuelStock.staticFuelStock.fuel <= (FuelStock.staticFuelStock.maxFuel/3) && FoodStock.staticFoodStock.food > (FoodStock.staticFoodStock.maxFood / 3))
+        {
+            if (RouteManager.Index == 0)
+            {
+                WarningFuelDroite.SetActive(false);
+                WarningFuelGauche.SetActive(true);
+                WarningFoodGauche.SetActive(false);
+                WarningFoodDroite.SetActive(false);
+                WarningBothGauche.SetActive(false);
+                WarningBothDroite.SetActive(false);
+            }
+            else if (RouteManager.Index == 1)
+            {
+                WarningFuelGauche.SetActive(false);
+                WarningFuelDroite.SetActive(true);
+                WarningFoodGauche.SetActive(false);
+                WarningFoodDroite.SetActive(false);
+                WarningBothGauche.SetActive(false);
+                WarningBothDroite.SetActive(false);
+            }
+        }
+
+        if (FoodStock.staticFoodStock.food <= (FoodStock.staticFoodStock.maxFood / 3) && FuelStock.staticFuelStock.fuel > (FuelStock.staticFuelStock.maxFuel / 3))
+        {
+            if (RouteManager.Index == 0)
+            {
+                WarningFuelDroite.SetActive(false);
+                WarningFuelGauche.SetActive(false);
+                WarningFoodGauche.SetActive(true);
+                WarningFoodDroite.SetActive(false);
+                WarningBothGauche.SetActive(false);
+                WarningBothDroite.SetActive(false);
+            }
+            else if (RouteManager.Index == 1)
+            {
+                WarningFuelGauche.SetActive(false);
+                WarningFuelDroite.SetActive(false);
+                WarningFoodGauche.SetActive(false);
+                WarningFoodDroite.SetActive(true);
+                WarningBothGauche.SetActive(false);
+                WarningBothDroite.SetActive(false);
+            }
+        }
+
+        if (FoodStock.staticFoodStock.food <= (FoodStock.staticFoodStock.maxFood / 3) && FuelStock.staticFuelStock.fuel <= (FuelStock.staticFuelStock.maxFuel / 3))
+        {
+            if (RouteManager.Index == 0)
+            {
+                WarningFuelDroite.SetActive(false);
+                WarningFuelGauche.SetActive(false);
+                WarningFoodGauche.SetActive(false);
+                WarningFoodDroite.SetActive(false);
+                WarningBothGauche.SetActive(true);
+                WarningBothDroite.SetActive(false);
+            }
+            else if (RouteManager.Index == 1)
+            {
+                WarningFuelGauche.SetActive(false);
+                WarningFuelDroite.SetActive(false);
+                WarningFoodGauche.SetActive(false);
+                WarningFoodDroite.SetActive(false);
+                WarningBothGauche.SetActive(false);
+                WarningBothDroite.SetActive(true);
+            }
+        }
+    }
 
     void Awake()
     {
