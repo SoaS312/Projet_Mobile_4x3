d160d7c84060f73a59b7005ef6786afc411d2e8c
Joris <j.fabre@rubika-edu.com>
2019-06-13 10:56:01 +0200

    Doot

---
+++ Assets/LaunchGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchGame : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    void LaunchGameAfterAnim()
+    {
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Single);
+    }
+}
